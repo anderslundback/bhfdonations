cd65e3cbcb9ef89a99db591ee3ced63cc553d2f2
Anders <lundback.anders@gmail.com>
2018-02-16 18:29:57 +0000

    Fetch donations and console log them

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 Simple donation ticker for the British Heart Foundation.
 
 Displays donations using the API from justgiving.com
+
+Instructions: 
+
+1) clone the repository and npm install
+2) in `config.js` add your `{apiKey}`
+3) `npm start` to start the local webpack devserver
+4) the app is live on http://localhost:3000
--- src/App.js
+++ src/App.js
@@ -1,18 +1,66 @@
 import React, { Component } from 'react';
 import bhfLogo from './assets/images/bhflogo140x140.jpg';
 import './App.css';
+import './config.js';
+import request from './request.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      donations: [],
+      CHARITY_ID: 183092, // Charity ID, default: BHF
+      API_KEY: '' // Enter your API KEY here
+    };
+  }
+
+  renderHeader() {
+    return (
+      <header className="App-header">
+        <img src={bhfLogo} className="App-logo" alt="logo" />
+        <h1 className="App-title">British Heart Foundation</h1>
+      </header>
+    )
+  }
+
+  renderDonations() {
+    const { error, isLoaded, donations } = this.state;
+    if(error) {
+      return <div>Error fetching donations: {error.message}</div>
+    } else if (!isLoaded) {
+      return <div>Loading donations...</div>;
+    } else {
+      console.log(JSON.stringify(donations));
+    }
+  }
+
+  fetchDonations(apiKey, charityId) {
+    return request({
+      url: `/${apiKey}/v1/charity/${charityId}/donations`,
+      method: 'GET'
+    });
+  }
+  
+  componentDidMount() {
+    // To do: render donations elegantly, for now let's console log them
+    console.log(this.fetchDonations(this.state.API_KEY, this.state.CHARITY_ID));
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={bhfLogo} className="App-logo" alt="logo" />
-          <h1 className="App-title">British Heart Foundation</h1>
-        </header>
-        <p className="App-intro">
-          Donations will appear here!
-        </p>
+        {this.renderHeader()}
+
+        <div className="card card-body">
+          <div className="card-title"><h3>Latest donations</h3></div>
+          <div className="card-text" id="donationResult">
+            {/* To do: Display donations here */}
+            {this.renderDonations()}
+          </div>
+        </div>
       </div>
     );
   }
